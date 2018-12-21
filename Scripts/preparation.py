f260903e7dba8db46e3e8373e8af6adaa5ab6f34
ai-banzai <mikroself@gmail.com>
2018-12-22 00:38:04 +0100

    Initial commit

---
+++ Scripts/preparation.py
@@ -0,0 +1,29 @@
+import os.path
+import tensorflow as tf
+import scipy
+import skimage
+
+
+def load_data(data_directory):
+    directories = [d for d in os.listdir(data_directory) 
+                   if os.path.isdir(os.path.join(data_directory, d))]
+    labels = []
+    images = []
+    for d in directories:
+        label_directory = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_directory, f) 
+                      for f in os.listdir(label_directory) 
+                      if f.endswith(".ppm")]
+        for f in file_names:
+            images.append(skimage.data.imread(f))
+            labels.append(int(d))
+    return images, labels
+
+ROOT_PATH = "C:/Users/mikro/Documents/tensorflow/tensorflow-tutorial"
+train_data_directory = ROOT_PATH + "/TrafficSigns/Training"
+test_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Testing")
+
+images, labels = load_data(train_data_directory)
+
+print(images, labels)
+
